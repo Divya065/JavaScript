5a64b71ad0a8f4a0f84e1c856cb001e85c44fb64
Divya065 <parashardivy@gmail.com>
2024-12-26 12:23:04 +0530

    new

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// function sayHello(){alert("HelloWorld")}
+// function sayHello(){
+// alert("HelloWorld")}
 // document.write('<h1>This my first javascript page</h1>')
 // var pi=3.14;
 // var person="are you GOD";
@@ -6,8 +7,11 @@
 // document.write(pi+"<br>");
 // document.write(person+"<br>");
 // document.write(answer+"<br>");
+
+// function myFunction(){
 // var carname="Welcome";
 // document.getElementById("demo").innerHTML=carname;
+// }
 // var num=10;
 // console.log(num);
 // console.log(typeof(num));
@@ -23,6 +27,7 @@
 // var name=undefined;
 // console.log(num);
 // console.log(typeof(num));
+
 // var person={
 //     firstName:"John",
 //     lastName:"Doe",
@@ -178,7 +183,185 @@
 // {
 //     alert("Hello World");
 // }
-function sayHello(name,age)
+// function sayHello(name,age)
+// {
+//     document.write(name+" is "+age+" years old");
+// }
+
+// function add(a,b)
+// {
+//     console.log(a+b);
+//     document.write(a+b);
+// }
+// add(10,20);
+// var res=mul(12,30);
+// function mul(x,y)
+// {
+//     return x*y;
+// }
+// document.write(res);
+
+// const square=function(number)
+// {
+//     return number*number;
+// };
+// const x=square(4);
+// console.log(x);
+// function myFunction(g1,g2)
+// {
+//     return g1/g2;
+// }
+
+// const value=myFunction(8,2);
+// console.log(value)
+// function add(a,b)
+// {
+//     console.log(a+b);
+// }
+// add(10,20);
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// };
+// add(102,20);
+// var add3=(a,b)=>console.log(a+b);
+// add3(30,20);
+
+// var a=100;
+// var b=13;
+// var c=10;
+// var linebreak="<br/>";
+// document.write("a+b-c=");
+// result=a+b-c;
+// document.write(result);
+// document.write(linebreak);
+// document.write("a+b=");
+// result=a+b;
+// document.write(result);
+// document.write(linebreak);
+// document.write("a-b=");
+// result=a-b;
+// document.write(result);
+// document
+
+// function myFunction()
+// {
+//     var x=5+5;
+//     var y="5"+5;
+//     var z="Hello"+5;
+//     var demoP=document.getElementById("demo");
+//     demoP.innerHTML=x+"<br/>"+y+"<br/>"+z;
+// }
+
+// var a=10;
+// var b=20;
+// var linebreak="<br/>";
+
+// document.write("(a==b)=>");
+// result=(a==b);
+// document.write(result);
+// document.write(linebreak);
+ 
+// document.write("(a<b)=>");
+// result=(a<b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a>b)=>");
+// result=(a>b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a!=b)=>");
+// result=(a!=b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a>=b)=>");
+// result=(a>=b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a<=b)=>");
+// result=(a<=b);
+// document.write(result);
+// document.write(linebreak);
+
+// let x=6;
+// let y=3;
+
+// console.log(x==10 && y<1);
+// console.log(x==6 || y==5);
+// console.log(!(x==y));
+
+// var age=20
+// var voteable=(age<18)?"Too Young":"Old Enough";
+// document.write(voteable);
+
+// var myCollection=[
+//     1,
+//     "meera mam",
+//     true,
+//     {
+//         name:"Sanjeev",
+//         age:30
+//     },
+//     function(name){
+//         console.log(name);
+//     }
+//     ["abhishek","bacchan","amitabh","bacchan"]
+// ]
+
+// function information(firstName,lastName,language){
+//     if (arguments.length==3){
+//         console.log(firstName);
+//         console.log(lastName);
+//         console.log(language);
+//     }
+// }
+// information();
+// information('Divya','Parashar','Sharma')
+
+// function over(){
+//     document.getElementById('mytext')="#000";
+// }
+// function mout(){
+//     document.getElementById('mytext').style.color="magenta";
+// }
+
+// var retVal=confirm("Do you want to confirm ?");if(retVal==true){
+//     alert("User wants to continue!");
+// }else{
+//     alert("User does not want to continue!");
+// }
+
+// var retVal=prompt("Enter your name :","your name");
+// alert("Your have entered :"+retVal);
+
+// var name=prompt("Enter Your Name","Name");
+// document.write("<h2>Hello "+name+"</h2>")
+
+var question="What is 10+10";
+var answer=20;
+var correct='<img src="1.jpg" height="150" width="250">';
+var incorrect='<img src="love.jpg" height="150" width="250">';
+var response=prompt(question,"0");
+for(count=0;count<3;count++)
 {
-    document.write(name+"is"+age+"years old");
+    if(response!=answer)
+    {
+        confirm("WebTransportDatagramDuplexStream, Press OK for another chance");
+        response=prompt(question,"0");
+        if(count==1)
+        {
+            alert("Better luck next time");
+        }
+    }
+        else
+    {
+        alert("Great! you are right");
+        count=3;
+    }
 }
+var output=(response==answer)?correct:incorrect;
+document.write("</br>");
+document.write(output);
